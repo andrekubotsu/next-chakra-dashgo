c729a59cfc7ea6b15b54ca14688efa01ab627bf7
Andre Kubotsu <contato201@gmail.com>
2021-12-24 12:20:07 -0300

    Sidebar responsiveness and drawer added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
-import { AppProps } from 'next/app'
+import { AppProps } from "next/app";
 
 // importing chakra ui provider for themming purposes
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '../styles/theme'
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
 
-
-function MyApp({ Component, pageProps }:AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <SidebarDrawerProvider>
         <Component {...pageProps} />
+      </SidebarDrawerProvider>
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
