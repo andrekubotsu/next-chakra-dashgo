5d46bdff787f936ab9fbfc163deb192e2d840610
Andre Kubotsu <contato201@gmail.com>
2021-12-24 11:52:25 -0300

    Header responsiveness

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,19 @@
 import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Andre Kubotsu</Text>
-        <Text color="gray.300">contato201@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Andre Kubotsu</Text>
+          <Text color="gray.300">contato201@gmail.com</Text>
+        </Box>
+      )}
+
       <Avatar
         size="md"
         name="Andre Kubotsu"
